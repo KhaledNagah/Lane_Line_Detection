ad400beff2a777dd69b0b9f5b4ab1cd2f4129323
Dina Abdul-Rasoul <73671062+dinaabdulrasoul@users.noreply.github.com>
2022-04-20 01:28:11 +0200

    Find lane pixels using the polynomial function

---
+++ find_lane_pixels_using_prev_poly.py
@@ -0,0 +1,28 @@
+import numpy as np
+import cv2
+
+def find_lane_pixels_using_prev_poly(binary_warped):
+    """
+    Find lane pixels using the polynomial
+    """
+    global prev_left_fit
+    global prev_right_fit
+    # width of the margin around the previous polynomial to search
+    margin = 100
+    # Grab activated pixels
+    nonzero = binary_warped.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])    
+    ### Set the area of search based on activated x-values ###
+    ### within the +/- margin of our polynomial function ###
+    left_lane_inds = ((nonzerox > (prev_left_fit[0]*(nonzeroy**2) + prev_left_fit[1]*nonzeroy + 
+                    prev_left_fit[2] - margin)) & (nonzerox < (prev_left_fit[0]*(nonzeroy**2) + 
+                    prev_left_fit[1]*nonzeroy + prev_left_fit[2] + margin))).nonzero()[0]
+    right_lane_inds = ((nonzerox > (prev_right_fit[0]*(nonzeroy**2) + prev_right_fit[1]*nonzeroy + 
+                    prev_right_fit[2] - margin)) & (nonzerox < (prev_right_fit[0]*(nonzeroy**2) + 
+                    prev_right_fit[1]*nonzeroy + prev_right_fit[2] + margin))).nonzero()[0]
+    # Again, extract left and right line pixel positions
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds] 
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds]
