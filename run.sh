e268c222ae26f00f3d670f8397dbde64dc6b01ab
git-student <alyysab3@gmail.com>
2022-04-20 06:15:04 +0200

    Wrote the running bash script

--- run.sh
+++ run.sh
@@ -1,1 +1,14 @@
 #!/bin/sh
+
+# Getting the needed inputs from the terminal
+echo -n "Source File Path: "
+read input_path
+
+echo -n "Output File Path: "
+read output_path
+
+echo -n "Operation Mode: "
+read mode
+
+# Calling the Python3 interpreter and passing the arguments to the main program
+python3 main.py $input_path $output_path $mode
