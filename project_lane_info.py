8e0497309cd3960ad543b716ee8e81d7b3b6e6c7
git-student <alyysab3@gmail.com>
2022-04-20 04:37:45 +0200

    Added the radius of curvature text and central offset to the output image

--- project_lane_info.py
+++ project_lane_info.py
@@ -9,28 +9,30 @@ def project_lane_info(img, binary_warped, ploty, left_fitx, right_fitx, M_inv, l
     # Create an image to draw the lines on
     warp_zero = np.zeros_like(binary_warped).astype(np.uint8)
     color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
-    
+
     # Recast the x and y points into usable format for cv2.fillPoly()
     pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
     pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
 
 
     pts = np.hstack((pts_left, pts_right))
-    
+
     # Draw the lane onto the warped blank image
     cv2.fillPoly(color_warp, np.int_([pts]), (0,255, 0))
-    
+
     # Warp the blank back to original image space using inverse perspective matrix (Minv)
     newwarp = cv2.warpPerspective(color_warp, M_inv, (img.shape[1], img.shape[0]))
-   
+
     newwarp=newwarp&temp
     ####################3##################
     #cv2.imshow("newwarp",newwarp)
     #cv2.waitKey(0)
     ####################3##################
-    
+
     # Combine the result with the original image
     out_img = cv2.addWeighted(img, 1, newwarp, 0.3, 0)
 
-    return out_img,newwarp
+    cv2.putText(out_img,'Curve Radius [m]: '+str((left_curverad+right_curverad)/2)[:7],(40,70), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.6, (255,255,255),2,cv2.LINE_AA)
+    cv2.putText(out_img,'Center Offset [m]: '+str(veh_pos)[:7],(40,150), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.6,(255,255,255),2,cv2.LINE_AA)
 
+    return out_img,newwarp
