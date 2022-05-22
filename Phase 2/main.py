b893f5061261585a8fc6fde640bc84089c9d2d3a
KhaledNagah <54414359+KhaledNagah@users.noreply.github.com>
2022-05-23 00:03:46 +0200

    Selecting required train features
    Select required train features (HOG and color features)

--- Phase 2/main.py
+++ Phase 2/main.py
@@ -1,1 +1,56 @@
+import matplotlib.image as mpimg
+import numpy as np
+import cv2
+import glob
+import random
+from skimage.feature import hog
+from scipy.ndimage import label
+from moviepy.editor import VideoFileClip
+import queue
+import time
+from sklearn.svm import LinearSVC
+from sklearn.model_selection  import train_test_split
+from sklearn.utils import shuffle
+from sklearn.preprocessing import StandardScaler
+import pickle
+import sys
+import os
+#####################################################################################################################################################
+"""STEP 1: Creating the model for the classifier"""
 
+"""1A: Select required train features (HOG and color features)"""
+def get_hog_features(img, orient, pix_per_cell, cell_per_block, 
+                        vis=False, feature_vec=True):
+    """
+    Return the hog features of the given input image
+    Call with two outputs if vis==True"""
+    if vis == True:
+        features, hog_image = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
+                                  cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=True, 
+                                  visualize=vis, feature_vector=feature_vec)
+        return features, hog_image
+    # Otherwise call with one output
+    else:      
+        features = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
+                       cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=True, 
+                       visualize=vis, feature_vector=feature_vec)
+        return features
+
+def bin_spatial(img, size=(16, 16)):
+    color1 = cv2.resize(img[:,:,0], size).ravel()
+    color2 = cv2.resize(img[:,:,1], size).ravel()
+    color3 = cv2.resize(img[:,:,2], size).ravel()
+    return np.hstack((color1, color2, color3))
+
+## Define a function to compute color histogram features 
+## NEED TO CHANGE bins_range if reading .png files with mpimg!
+def color_hist(img, nbins=32, bins_range=(0, 256)):
+    # Compute the histogram of the color channels separately
+    channel1_hist = np.histogram(img[:,:,0], bins=nbins, range=bins_range)
+    channel2_hist = np.histogram(img[:,:,1], bins=nbins, range=bins_range)
+    channel3_hist = np.histogram(img[:,:,2], bins=nbins, range=bins_range)
+    # Concatenate the histograms into a single feature vector
+    hist_features = np.concatenate((channel1_hist[0], channel2_hist[0], channel3_hist[0]))
+    # Return the individual histograms, bin_centers and feature vector
+    return hist_features
+## Have this function call bin_spatial() and color_hist()
