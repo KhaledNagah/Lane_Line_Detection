69e3d9cd6276c2a9eb75101c988a54dd97612c3a
Dina Abdul-Rasoul <73671062+dinaabdulrasoul@users.noreply.github.com>
2022-05-23 00:50:34 +0200

    classifier training

--- Phase 2/main.py
+++ Phase 2/main.py
@@ -124,3 +124,87 @@ def read_images(vehicles_dir,non_vehicles_dir):
             notcars.append(image)
     
     return cars, notcars
+  
+"""1D: Train the classifier on the features and save the training model"""
+
+def train_classifier():
+    colorspace = 'YUV' # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
+    orient = 9
+    pix_per_cell = 8
+    cell_per_block = 2
+    hog_channel = 'ALL' # Can be 0, 1, 2, or "ALL"
+    spatial_size=(32, 32)
+    hist_bins=32
+    
+    t=time.time()
+    
+    cars,notcars=read_images('./dataset/vehicles/','./dataset/non-vehicles/')
+    car_features = extract_features(cars, cspace=colorspace, orient=orient, 
+                            pix_per_cell=pix_per_cell, cell_per_block=cell_per_block, 
+                            hog_channel=hog_channel, hist_bins=hist_bins)
+    notcar_features = extract_features(notcars, cspace=colorspace, orient=orient, 
+                            pix_per_cell=pix_per_cell, cell_per_block=cell_per_block, 
+                            hog_channel=hog_channel, hist_bins=hist_bins)
+    t2 = time.time()
+    print(round(t2-t, 2), 'Seconds to extract HOG features...')
+    # Create an array stack of feature vectors
+    X = np.vstack((car_features, notcar_features)).astype(np.float64) 
+    # Fit a per-column scaler
+    X_scaler = StandardScaler().fit(X)
+    # Apply the scaler to X
+    scaled_X = X_scaler.transform(X)
+    
+    # Define the labels vector
+    y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
+    
+    
+    # Split up data into randomized training and test sets
+    rand_state = np.random.randint(0, 100)
+    X_train, X_test, y_train, y_test = train_test_split(
+        scaled_X, y, test_size=0.15, random_state=rand_state)
+    
+    print('Using:',orient,'orientations',pix_per_cell,
+        'pixels per cell and', cell_per_block,'cells per block')
+    print('Feature vector length:', len(X_train[0]))
+    
+    # Use a linear SVC X_scaler
+    svc = LinearSVC()
+    # Check the training time for the SVC
+    t=time.time()
+    svc.fit(X_train, y_train)
+    t2 = time.time()
+    print(round(t2-t, 2), 'Seconds to train SVC...')
+    
+    
+    # Check the score of the SVC
+    print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
+    # Check the prediction time for a single sample
+    t=time.time()
+    n_predict = 10
+    print('My SVC predicts: ', svc.predict(X_test[0:n_predict]))
+    print('For these',n_predict, 'labels: ', y_test[0:n_predict])
+    t2 = time.time()
+    print(round(t2-t, 5), 'Seconds to predict', n_predict,'labels with SVC')
+    
+    
+    """Save the training model"""
+    
+    #Pickle the data as it takes a lot of time to generate it
+    
+    data_file = './svc_pickle.p'
+    
+    if not os.path.isfile(data_file):
+        with open(data_file, 'wb') as pfile:
+            pickle.dump(
+                {
+                    'svc': svc,
+                    'scaler': X_scaler,
+                    'orient': orient,
+                    'pix_per_cell': pix_per_cell,
+                    'cell_per_block': cell_per_block,
+                    'spatial_size': spatial_size,
+                    'hist_bins': hist_bins
+                    
+                },
+                pfile, pickle.HIGHEST_PROTOCOL) 
+    print('Data saved in pickle file')
