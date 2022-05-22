96d8e550e21f6433e46451657762aaf4bf042a8f
Ahmed Osama Mohamed <42383306+ahmedosamataha@users.noreply.github.com>
2022-05-23 00:54:01 +0200

    Update main.py

--- Phase 2/main.py
+++ Phase 2/main.py
@@ -208,3 +208,107 @@ def train_classifier():
                 },
                 pfile, pickle.HIGHEST_PROTOCOL) 
     print('Data saved in pickle file')
+ 
+
+
+
+
+    
+"""Step 2: Detect vehicles"""
+"""2A: Load the training model"""
+print('Loading Classifier parameters...')
+dist_pickle = pickle.load( open("svc_pickle.p", "rb" ) )
+svc = dist_pickle["svc"]
+X_scaler = dist_pickle["scaler"]
+orient = dist_pickle["orient"]
+pix_per_cell = dist_pickle["pix_per_cell"]
+cell_per_block = dist_pickle["cell_per_block"]
+spatial_size = dist_pickle["spatial_size"]
+hist_bins = dist_pickle["hist_bins"]
+
+print('Loading is done!')
+
+
+
+"""2B: Do a sliding window search with different window scales tp detect bounding boxes """
+
+#Select various color spaces
+def convert_color(img, conv='RGB2YCrCb'):
+    """
+    Convert the image from one color space to the other
+    """
+    if conv == 'RGB2YCrCb':
+        return cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
+    if conv == 'BGR2YCrCb':
+        return cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
+    if conv == 'RGB2LUV':
+        return cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
+    if conv == 'RGB2HLS':
+        return cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+    if conv == 'RGB2HSV':
+        return cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    if conv == 'Gray':
+        return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    if conv == 'RGB2YUV':
+        return cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+
+# Define a single function that can extract features using hog sub-sampling and make predictions
+def find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size, hist_bins, vis_bboxes = False):
+    
+    draw_img = np.copy(img)
+    xstart = int(img.shape[1]/5)
+    xstop = img.shape[1]
+    img_tosearch = img[ystart:ystop, xstart:xstop,:]
+    ctrans_tosearch = convert_color(img_tosearch, conv='RGB2YUV')
+    if scale != 1:
+        imshape = ctrans_tosearch.shape
+        ctrans_tosearch = cv2.resize(ctrans_tosearch, (int(imshape[1]/scale), int(imshape[0]/scale)))
+
+    ch1 = ctrans_tosearch[:,:,0]
+    ch2 = ctrans_tosearch[:,:,1]
+    ch3 = ctrans_tosearch[:,:,2]
+
+    # Define blocks and steps as above
+    nxblocks = (ch1.shape[1] // pix_per_cell) - cell_per_block + 1
+    nyblocks = (ch1.shape[0] // pix_per_cell) - cell_per_block + 1 
+    nfeat_per_block = orient*cell_per_block**2
+    
+    # 64 was the orginal sampling rate, with 8 cells and 8 pix per cell
+    window = 64
+    nblocks_per_window = (window // pix_per_cell) - cell_per_block + 1
+    cells_per_step = 2  # Instead of overlap, define how many cells to step
+    nxsteps = (nxblocks - nblocks_per_window) // cells_per_step
+    nysteps = (nyblocks - nblocks_per_window) // cells_per_step
+
+    # Compute individual channel HOG features for the entire image
+    hog1 = get_hog_features(ch1, orient, pix_per_cell, cell_per_block, feature_vec=False)
+    hog2 = get_hog_features(ch2, orient, pix_per_cell, cell_per_block, feature_vec=False)
+    hog3 = get_hog_features(ch3, orient, pix_per_cell, cell_per_block, feature_vec=False)
+    rectangles = []
+
+    for xb in range(nxsteps):
+        for yb in range(nysteps):
+            ypos = yb*cells_per_step
+            xpos = xb*cells_per_step
+            
+            # Extract HOG for this patch
+            hog_feat1 = hog1[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel() 
+            hog_feat2 = hog2[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel() 
+            hog_feat3 = hog3[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel() 
+            hog_features = np.hstack((hog_feat1, hog_feat2, hog_feat3)).reshape(1, -1)
+            
+            xleft = xpos*pix_per_cell
+            ytop = ypos*pix_per_cell
+
+
+            test_features = X_scaler.transform(hog_features)   
+            test_prediction = svc.predict(test_features)
+            
+            if test_prediction == 1 or vis_bboxes == True:
+                xbox_left = int(xleft*scale)
+                ytop_draw = int(ytop*scale)
+                win_draw = int(window*scale)
+                rectangles.append(((xbox_left+xstart, ytop_draw+ystart),(xbox_left+win_draw+xstart,ytop_draw+win_draw+ystart)))
+                              
+    return rectangles
+    
